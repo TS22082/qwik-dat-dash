0bbaa50127a5523d945842a2ef478d4e0fca381d
Thomas Li-Smith <geekcentric@Thomass-MacBook-Pro-2.local>
2024-07-28 20:54:08 -0700

    split into hooks

--- src/routes/auth/sso-redirect/index.tsx
+++ src/routes/auth/sso-redirect/index.tsx
@@ -1,38 +1,8 @@
-import { component$, useTask$ } from "@builder.io/qwik";
-import { useLocation } from "@builder.io/qwik-city";
+import { component$ } from "@builder.io/qwik";
+import useSsoRedirect from "./useSsoRedirect";
 
 export default component$(() => {
-  const loc = useLocation();
-
-  useTask$(async () => {
-    if (!loc.prevUrl?.searchParams) return;
-    const code = loc.url.searchParams.get("code");
-
-    try {
-      const response = await fetch(`http://127.0.0.1:8080/api/github/gh_login?code=${code}`);
-
-      if (response.status === 200) {
-        const data = await response.json();
-
-        if (!data?.token) {
-          return;
-        }
-
-        const { token } = data;
-        localStorage.setItem("accessToken", token);
-      }
-    } catch (err) {
-      console.log("this is the error ==>", err);
-    }
-  })
-
-  
-
-  const handleRequest = async () => {
-    
-  };
-
-  handleRequest();
+  useSsoRedirect();
 
   return <h1>Processing login data</h1>;
 });
---
+++ src/routes/auth/sso-redirect/useSsoRedirect.ts
@@ -0,0 +1,33 @@
+import { useTask$ } from "@builder.io/qwik";
+import { useLocation } from "@builder.io/qwik-city";
+
+
+const useSsoRedirect = () => {
+  const loc = useLocation();
+
+  useTask$(async () => {
+    if (!loc.prevUrl?.searchParams) return;
+    const code = loc.url.searchParams.get("code");
+
+    try {
+      const response = await fetch(
+        `http://127.0.0.1:8080/api/github/gh_login?code=${code}`
+      );
+
+      if (response.status === 200) {
+        const data = await response.json();
+
+        if (!data?.token) {
+          return;
+        }
+
+        const { token } = data;
+        localStorage.setItem("accessToken", token);
+      }
+    } catch (err) {
+      console.log("this is the error ==>", err);
+    }
+  });
+}
+
+export default useSsoRedirect
